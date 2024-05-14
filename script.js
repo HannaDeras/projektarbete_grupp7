058efd685542d82fd580f35a9699aaaab2b4b184
HannaDeras <120727382+HannaDeras@users.noreply.github.com>
2024-05-14 11:27:51 +0200

    små fix

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const urlSCB = "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/TK/TK1001/TK1001A/FordonTrafik";
 
 
+// Knapp som tar användaren tillbaka till toppen
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
 //Här hämtar vi data om hur många personer i alla län mellan 2012-2022 som åkte personbil
 const querySCB = {
     "query": [
